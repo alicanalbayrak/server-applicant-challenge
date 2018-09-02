fc4985ef5f1a885b4e4d345e98aea0f589703639
alicana <albayrakalican@gmail.com>
2018-09-02 20:24:04 +0300

    manufacturer name updated to be unique

--- src/main/java/com/mytaxi/domain/Manufacturer.java
+++ src/main/java/com/mytaxi/domain/Manufacturer.java
@@ -4,16 +4,36 @@ import com.mytaxi.domain.shared.BaseEntity;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
-@Table(name = "manufacturer")
+@Table(name = "manufacturer",
+    uniqueConstraints = @UniqueConstraint(name = "uc_manufacturer_name", columnNames = {"name"})
+)
 public class Manufacturer extends BaseEntity<Manufacturer>
 {
     @Column(name = "Name")
     private String name;
+
+
+    private Manufacturer()
+    {
+    }
+
+
+    private Manufacturer(String name)
+    {
+        this.name = name;
+    }
+
+
+    public static Manufacturer createManufacturer(String name)
+    {
+        return new Manufacturer(name);
+    }
 
 }
