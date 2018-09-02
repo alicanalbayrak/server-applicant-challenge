2ab521c6446142e8f7788f74ce46699444aaf632
alicana <albayrakalican@gmail.com>
2018-09-02 13:37:47 +0300

    Initial crud repo integration test added

---
+++ src/test/java/com/mytaxi/infrastructure/CrudDriverRepositoryTest.java
@@ -0,0 +1,83 @@
+package com.mytaxi.infrastructure;
+
+import com.mytaxi.domain.Driver;
+import com.mytaxi.domain.OnlineStatus;
+import java.util.List;
+import java.util.Optional;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+public class CrudDriverRepositoryTest
+{
+
+    @Autowired
+    private CrudDriverRepository crudDriverRepository;
+
+
+    @Test
+    public void testNumberOfDrivers()
+    {
+        long numberOfDrivers = crudDriverRepository.count();
+
+        assertThat(numberOfDrivers).isEqualTo(8);
+    }
+
+
+    @Test
+    public void testFindOnlineDrivers()
+    {
+        List<Driver> onlineDrivers = crudDriverRepository.findByOnlineStatus(OnlineStatus.ONLINE);
+
+        assertNotNull(onlineDrivers);
+        assertEquals(4, onlineDrivers.size());
+    }
+
+
+    @Test
+    public void testFindOfflineDrivers()
+    {
+        List<Driver> offlineDrivers = crudDriverRepository.findByOnlineStatus(OnlineStatus.OFFLINE);
+
+        assertNotNull(offlineDrivers);
+        assertEquals(4, offlineDrivers.size());
+    }
+
+
+    @Test
+    public void testFindDriver()
+    {
+        long driverId = 1L;
+
+        Optional<Driver> driverOptional = crudDriverRepository.findById(driverId);
+
+        assertThat(driverOptional).isNotEmpty();
+
+        Driver driver = driverOptional.get();
+        assertThat(driver.getUsername()).isEqualTo("driver01");
+        assertThat(driver.getPassword()).isEqualTo("driver01pw");
+        assertThat(driver.getOnlineStatus()).isEqualTo(OnlineStatus.OFFLINE);
+        assertThat(driver.getDeleted()).isFalse();
+        assertThat(driver.getCoordinate()).isNull();
+    }
+
+
+    @Test
+    public void testCreateDriver()
+    {
+        Driver driver = crudDriverRepository.save(new Driver("tmpDriver", "tmpDriverPassword"));
+        assertThat(driver).isNotNull();
+        assertThat(driver.getId()).isEqualTo(crudDriverRepository.findById(driver.getId()).get().getId());
+        assertThat(crudDriverRepository.count()).isEqualTo(9);
+    }
+
+
+}
