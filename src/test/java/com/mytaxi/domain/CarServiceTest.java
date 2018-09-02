9854b793f9f780193a8401bdba469c74024cd84f
alicana <albayrakalican@gmail.com>
2018-09-03 00:20:07 +0300

    CarService unit test updated

--- src/test/java/com/mytaxi/domain/CarServiceTest.java
+++ src/test/java/com/mytaxi/domain/CarServiceTest.java
@@ -1,5 +1,7 @@
 package com.mytaxi.domain;
 
+import java.time.ZonedDateTime;
+import java.util.Optional;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -7,10 +9,15 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 class CarServiceTest
 {
 
+    @Mock
+    private ManufacturerRepository manufacturerRepository;
 
     @Mock
     private CarRepository carRepository;
@@ -30,6 +37,9 @@ class CarServiceTest
     void testFindCarById() throws EntityNotFoundException
     {
 
+        long carId = 1L;
+        when(carRepository.findById(carId)).thenReturn(Optional.of(createNewCar()));
+
         Car car = carService.find(1L);
 
         assertThat(car).isNotNull();
@@ -47,17 +57,13 @@ class CarServiceTest
     @Test
     void testCreateNewCar() throws ConstraintsViolationException
     {
-        Car car = carService.create(createNewCar());
 
-        assertThat(car).isNotNull();
-        assertThat(car.getId()).isNotNull();
-        assertThat(car.getConvertible()).isFalse();
-        assertThat(car.getEngineType()).isEqualTo(EngineType.DISEL);
-        assertThat(car.getLicensePlate()).isEqualTo(LicensePlate.of("06XYZ999"));
-        assertThat(car.getManufacturer().getName()).isEqualTo("Honda");
-        assertThat(car.getRating()).isEqualTo(Rating.of(0d));
-        assertThat(car.getSeatCount()).isEqualTo(SeatCount.of(4));
-        assertThat(car.getDeleted()).isFalse();
+        Car car = createNewCar();
+        when(carRepository.save(car)).thenReturn(car);
+
+        carService.create(createNewCar());
+
+        verify(carRepository, times(1)).save(car);
     }
 
 
@@ -66,7 +72,14 @@ class CarServiceTest
     {
         long carId = 1L;
 
+        Car car = createNewCar();
+        when(carRepository.findById(carId)).thenReturn(Optional.of(car));
+
         carService.delete(carId);
+
+        assertThat(car.getDeleted()).isTrue();
+
+        verify(carRepository, times(1)).findById(carId);
 
     }
 
@@ -76,20 +89,53 @@ class CarServiceTest
     {
 
         Car car = createNewCar();
+        when(carRepository.findById(2L)).thenReturn(Optional.of(car));
+        when(manufacturerRepository.findByName("Toyota")).thenReturn(Optional.of(Manufacturer.createManufacturer("Toyota")));
 
-        carService.update(car);
+        carService.update(createDifferentCar());
+
+        // these attributes should not ve overwritten!
+        assertThat(car.getId()).isEqualTo(1L);
+        assertThat(car.getDeleted()).isFalse();
+
+        assertThat(car.getConvertible()).isTrue();
+        assertThat(car.getEngineType()).isEqualTo(EngineType.GAS);
+        assertThat(car.getLicensePlate()).isEqualTo(LicensePlate.of("40AAB23"));
+        assertThat(car.getManufacturer().getName()).isEqualTo(Manufacturer.createManufacturer("Toyota").getName());
+        assertThat(car.getRating()).isEqualTo(Rating.of(0d));
+        assertThat(car.getSeatCount()).isEqualTo(SeatCount.of(4));
+
     }
 
 
     private static Car createNewCar()
     {
         return Car.builder()
+            .id(1L)
             .convertible(false)
             .engineType(EngineType.DISEL)
             .licensePlate(LicensePlate.of("06XYZ999"))
             .manufacturer(Manufacturer.createManufacturer("Honda"))
             .rating(Rating.of(0d))
             .seatCount(SeatCount.of(4))
+            .dateCreated(ZonedDateTime.now())
+            .deleted(false)
+            .build();
+    }
+
+
+    private static Car createDifferentCar()
+    {
+        return Car.builder()
+            .id(2L)
+            .convertible(true)
+            .engineType(EngineType.GAS)
+            .licensePlate(LicensePlate.of("40AAB23"))
+            .manufacturer(Manufacturer.createManufacturer("Toyota"))
+            .rating(Rating.of(0d))
+            .seatCount(SeatCount.of(4))
+            .dateCreated(ZonedDateTime.now())
+            .deleted(true)
             .build();
     }
 
